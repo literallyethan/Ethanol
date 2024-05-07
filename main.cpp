798452431f48c869d9c148ae6cb7502478a4b85a
Ethan Purser <epurse2@lsu.edu>
2024-05-06 20:54:42 -0500

    gives error when not given an etl file.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 #include <string>
 #include <fstream>
 
+void usage() {
+    using namespace std;
+
+    cout << "Usage: ethanol <filename>" << endl;
+}
+
 int main(int argc, char* argv[]) {
-    
-    std::cout << "Hello, world!" << std::endl;
+    using namespace std;
+
+    if (argc != 2) {
+        usage();
+        return 1;
+    }
+
+    std::string filename(argv[1]);
+    if (filename.length() < 5) {
+        cout << "Invalid filename." << endl;
+        return 1;
+    }
+
+    if ((filename.substr(filename.length() - 4, 4) != ".etl")) {
+        cout << "Not an .etl file." << endl;
+        return 1;
+    }
+
+
 
     return 0;
 }
